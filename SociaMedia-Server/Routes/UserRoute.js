0958f7d414a22a9c7255148c71ee8250ea7085da
Sohail Ali <alisohail2448@gmail.com>
2022-07-20 22:06:03 +0530

    1st task

--- SociaMedia-Server/Routes/UserRoute.js
+++ SociaMedia-Server/Routes/UserRoute.js
@@ -1,11 +1,14 @@
 import express from "express";
-import { deleteUser, followUser, getUser, UnFollowUser, updateUser } from "../Controllers/UserController.js";
+import { deleteUser, followUser, getUser, UnFollowUser, updateUser, getAllUsers } from "../Controllers/UserController.js";
+import authMiddleWare from "../MiddleWares/AuthMiddleWare.js";
 
 const router = express.Router();
 
+
+router.get('/', getAllUsers)
 router.get('/:id', getUser)
-router.put('/:id', updateUser)
-router.delete('/:id', deleteUser)
-router.put('/:id/follow', followUser)
-router.put('/:id/unfollow', UnFollowUser)
+router.put('/:id', authMiddleWare, updateUser)
+router.delete('/:id', authMiddleWare, deleteUser)
+router.put('/:id/follow', authMiddleWare, followUser)
+router.put('/:id/unfollow', authMiddleWare, UnFollowUser)
 export default router;
--- SocialMedia-Client/src/components/Posts/Posts.jsx
+++ SocialMedia-Client/src/components/Posts/Posts.jsx
@@ -4,23 +4,23 @@ import { PostsData } from '../../Data/PostsData'
 import Post from '../Post/Post'
 import { useSelector, useDispatch } from "react-redux";
 import { getTimelinePosts } from '../../Actions/PostAction';
+import {useParams} from "react-router-dom"  
 
 
 const Posts = () => {
   const { user } = useSelector((state) => state.authReducer.authData)
-  const { posts, loading } = useSelector((state) => state.postReducer);
+  let { posts, loading } = useSelector((state) => state.postReducer);
   const dispatch = useDispatch();
-
+const params =  useParams()
   useEffect(() => {
     dispatch(getTimelinePosts(user._id));
   }, []);
 
-
-
+ if(!posts) return "No Posts";
+ if(params.id) posts = posts.filter((post)=>post.userId == params.id)
   return (
     <div className="Posts">
-        {
-        loading ? "Fetching Posts..." : posts.map((post, id)=>{
+        {loading ? "Fetching Posts..." : posts.map((post, id)=>{
             return <Post data={post} id={id}/>
         })}
     </div>
